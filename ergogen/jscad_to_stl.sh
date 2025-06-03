a2377bcdccb7325f1b4616f16b9693969b39b8df
AnkushRoy-code <ankush3411111@gmail.com>
2025-06-03 22:45:04 +0530

    changed the cases folder to the root folder

--- ergogen/jscad_to_stl.sh
+++ ergogen/jscad_to_stl.sh
@@ -1,1 +1,10 @@
-ergogen . && for i in output/cases/*.jscad; do npx @jscad/cli@1 "$i" -of stla; done
+ergogen .
+
+for i in output/cases/*.jscad; do 
+    npx @jscad/cli@1 "$i" -of stla; 
+done;
+
+for j in output/cases/*.stl; do
+    cp "$j" "../cases/";
+done
+
